a3a812cf55056acfbfdfb8289b5c339ee6dfb76e
Aluno <aluno@alunos.ce.senac.br>
2026-06-03 16:41:53 -0300

    feat(model): Começando o model de noticia

--- backend/noticia/models.py
+++ backend/noticia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    status = models.BooleanField()
+    data_publicao = models.DateField()
+
+    
