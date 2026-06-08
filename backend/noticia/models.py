2e6d17c15c2f2b95b29d5fa7a717b5d10be18834
Aluno <aluno@alunos.ce.senac.br>
2026-06-08 14:15:19 -0300

    feat(relacionamento): Adicionando relacionamento entre categoria e notícia

--- backend/noticia/models.py
+++ backend/noticia/models.py
@@ -3,11 +3,22 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 class Noticia(models.Model):
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
     conteudo = models.TextField()
     ativa = models.BooleanField()
     data_publicao = models.DateField(auto_now_add=True)
+
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.SET_NULL, 
+        blank= True, 
+        null= True, 
+        related_name='produtos'
+        )
 
 
